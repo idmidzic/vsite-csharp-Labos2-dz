57d6d873fd684512d2265af5e5d4f229b73d6d77
Ida Midžić <idmidzic@vsite.hr>
2021-10-26 23:21:54 +0200

    Inicijalni commit
    DZ Labos 2, projekt zbroj 2 kompleksna broja.

---
+++ KompleksniBrojeviComplex/Complex.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace KompleksniBrojeviComplex
+{
+    struct Complex
+    {
+        public Complex(double Real, double Imaginary)
+        {
+            this.real = Real;
+            this.imaginary = Imaginary;
+
+        }
+
+        public double Real
+        {
+            get
+            {
+                return real;
+            }
+        }
+
+        public double Imaginary
+        {
+            get
+            {
+                return imaginary;
+            }
+        }
+
+        //operator za zbrajanje
+        public static Complex operator +(Complex a, Complex b)
+            {
+            return new Complex(a.real + b.real, a.imaginary + b.imaginary);
+
+        }
+
+        private readonly double real;
+        private readonly double imaginary;
+
+
+    }
+}
---
+++ KompleksniBrojeviComplex/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KompleksniBrojeviComplex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex a = new Complex(1, 2);
+            Complex b = new Complex(3, 4);
+            Complex c = a + b;
+
+            Console.WriteLine("Sum c.Real = " + c.Real);
+            Console.WriteLine("Sum c.Imaginary = " + c.Imaginary);
+        }
+    }
+}
